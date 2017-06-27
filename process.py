db215247d0c13081fa2dad5a42294db37bc103b6
Arindam Das Modak <iit2016036@iiita.ac.in>
2017-06-28 00:23:49 +0530

    Update process.py

--- process.py
+++ process.py
@@ -8,6 +8,10 @@ def process(bug,FPS,total_frames):
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_e:
+                classes.BugAttack.fire = not classes.BugAttack.fire
+                
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_d]:
@@ -23,16 +27,23 @@ def process(bug,FPS,total_frames):
         
     if keys[pygame.K_w]:
         bug.jumping = True
-
+    
     if keys[pygame.K_SPACE]:
-        p = classes.BugAttack(bug.rect.x,bug.rect.y, 40,20, "images/fire2.gif")
-        if classes.Bug.going_right:
-            p.velx = 8
+        def direction():
+            if classes.Bug.going_right:
+                p.velx = 8
+            else:
+                p.image = pygame.transform.flip(p.image, True, False)
+                p.velx = -8
+        if (classes.BugAttack.fire):
+            p = classes.BugAttack(bug.rect.x,bug.rect.y, "images/fire2.gif")
+            direction()
         else:
-            p.image = pygame.transform.flip(p.image, True, False)
-            p.velx = -8
-
+            p = classes.BugAttack(bug.rect.x,bug.rect.y, "images/bolt.gif")
+            direction()
+            
     spawn(FPS,total_frames)
+    collisions()
     
 def spawn(FPS,total_frames):
 
@@ -43,12 +54,22 @@ def spawn(FPS,total_frames):
         x = 1
         if r == 2:
             x = 640 - 40
-        classes.Fly(x,130,40,40,"images/fly.png")
+        classes.Fly(x,130,"images/fly.png")
 
+def collisions():
 
+    for fly in classes.Fly.List:
+        if pygame.sprite.spritecollide(fly, classes.BugAttack.List, False):
+            if classes.BugAttack.fire:
+                fly.health -= fly.half_health
+            else:
+                fly.velx = 0
 
-
-
+    for attack in classes.BugAttack.List:
+        if pygame.sprite.spritecollide(attack, classes.Fly.List, False):
+            attack.rect.x = 2 * -attack.rect.width
+            attack.destroy()
+            
 
 
 

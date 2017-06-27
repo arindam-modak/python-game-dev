9d1f43d19261cfa680eac730ebc7557b699e8902
Arindam Das Modak <iit2016036@iiita.ac.in>
2017-06-27 10:22:43 +0530

    baseclass plus motion function

--- classes.py
+++ classes.py
@@ -17,5 +17,29 @@ class BaseClass(pygame.sprite.Sprite):
         self.width = width
         self.height = height
 
+class Bug(BaseClass):
 
+    List = pygame.sprite.Group()
+    def __init__(self,x,y,width,height,image_string):
+
+        BaseClass.__init__(self,x,y,width,height,image_string)
+        Bug.List.add(self)
+        self.velx = 0
+        self.vely = 0
+        
+    def motion(self, SCREENWIDTH, SCREENHEIGHT):
+
+        predicted_locationx = self.rect.x + self.velx
+        predicted_locationy = self.rect.y + self.vely
+        if predicted_locationx < 0:
+            self.velx = 0
+        elif predicted_locationx + self.width > SCREENWIDTH:
+            self.velx = 0
+        elif predicted_locationy  < 0:
+            self.vely = 0
+        elif predicted_locationy + self.height > SCREENHEIGHT:
+            self.vely = 0
+        
+        self.rect.x += self.velx
+        self.rect.y += self.vely
         

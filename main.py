c14d5e785aaae8b72eedd1745e8dc939a2c182e9
Arindam Das Modak <iit2016036@iiita.ac.in>
2017-06-28 00:18:48 +0530

    update main.py

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 clock = pygame.time.Clock()
 FPS = 24
 
+background = pygame.image.load("images/forest.jpg")
 bug = Bug(0,SCREENHEIGHT - 40,40,40,"images/bug2right.png")
+fly = Fly(40,100,40,40,"images/fly.png")
+fly1 = Fly(40,100,40,40,"images/fly.png")
+fly2= Fly(40,100,40,40,"images/fly.png")
+fly3= Fly(40,100,40,40,"images/fly.png")
 
 while True:
 
     process(bug)
     
     bug.motion(SCREENWIDTH,SCREENHEIGHT)
-    screen.fill((255,0,200))
+    Fly.movement(SCREENWIDTH)
+    #screen.fill((255,0,200))
+    screen.blit(background,(0,0))
     BaseClass.allsprites.draw(screen)
     pygame.display.flip()
 

9d13622dd4a69f9cd7e3773390f9cd55c5287564
Arindam Das Modak <iit2016036@iiita.ac.in>
2017-06-28 00:22:02 +0530

    Update process.py

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
-import pygame, sys
+import pygame, sys, classes, random
 
-def process(bug):
+def process(bug,FPS,total_frames):
 
     # processing
     for event in pygame.event.get():
@@ -11,9 +11,11 @@ def process(bug):
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_d]:
+        classes.Bug.going_right = True
         bug.image = pygame.image.load("images/bug2right.png")
         bug.velx = 5
     elif keys[pygame.K_a]:
+        classes.Bug.going_right = False
         bug.image = pygame.image.load("images/bug2left.png")
         bug.velx = -5
     else:
@@ -21,9 +23,27 @@ def process(bug):
         
     if keys[pygame.K_w]:
         bug.jumping = True
-    
-    
 
+    if keys[pygame.K_SPACE]:
+        p = classes.BugAttack(bug.rect.x,bug.rect.y, 40,20, "images/fire2.gif")
+        if classes.Bug.going_right:
+            p.velx = 8
+        else:
+            p.image = pygame.transform.flip(p.image, True, False)
+            p.velx = -8
+
+    spawn(FPS,total_frames)
+    
+def spawn(FPS,total_frames):
+
+    four_seconds = FPS * 4
+
+    if total_frames % four_seconds==0:
+        r = random.randint(1,2)
+        x = 1
+        if r == 2:
+            x = 640 - 40
+        classes.Fly(x,130,40,40,"images/fly.png")
 
 
 

2605a90b0f84d31732105ac2d56911db1a002433
Arindam Das Modak <iit2016036@iiita.ac.in>
2017-06-29 00:17:15 +0530

    updated main.py

--- main.py
+++ main.py
@@ -15,16 +15,26 @@ bug = Bug(0,SCREENHEIGHT - 40,"images/bug2right.png")
 while True:
 
     process(bug,FPS,total_frames)
-    
-    bug.motion(SCREENWIDTH,SCREENHEIGHT)
-    #Fly.movement(SCREENWIDTH)
-    Fly.update_all(SCREENWIDTH)
-    BugAttack.movement()
-    total_frames += 1
-    #screen.fill((255,0,200))
-    screen.blit(background,(0,0))
-    BaseClass.allsprites.draw(screen)
-    BugAttack.List.draw(screen)
-    pygame.display.flip()
-
+    if Game.start:
+        bug.motion(SCREENWIDTH,SCREENHEIGHT)
+        #Fly.movement(SCREENWIDTH)
+        Fly.update_all(SCREENWIDTH,SCREENHEIGHT)
+        BugAttack.movement()
+        total_frames += 1
+        #screen.fill((255,0,200))
+        screen.blit(background,(0,0))
+        Fly.score(screen)
+        BaseClass.allsprites.draw(screen)
+        BugAttack.List.draw(screen)
+        pygame.display.flip()
+    else:
+        screen.fill((200,100,100))
+        font = pygame.font.SysFont(None, 40)
+        text = font.render("START ", True,  (0,255,50))
+        screen.blit(text,(SCREENWIDTH/2,SCREENHEIGHT/2))
+        text = font.render("(Press SPACE)", True,  (50,100,50))
+        screen.blit(text,(SCREENWIDTH/2 - 20,SCREENHEIGHT/2 + 60))
+        Fly.score(screen)
+        pygame.display.flip()
+        
     clock.tick(FPS)
